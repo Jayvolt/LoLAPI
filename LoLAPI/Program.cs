80c5ea3098e2a735d79de3a320cccfd297b96c73
Jayvolt <70279845+Jayvolt@users.noreply.github.com>
2021-11-29 16:00:44 -0500

    Initial commit

---
+++ LoLAPI/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MingweiSamuel.Camille;
+using MingweiSamuel.Camille.Enums;
+
+namespace LoLAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var riotApi = RiotApi.NewInstance("RGAPI-b9b8fb4e-8ced-4055-b218-1c0ec8203d7e");
+            var summoner = riotApi.SummonerV4.GetBySummonerName(Region.NA, "ChickenMessiah");
+            Console.WriteLine($"Summoner: {summoner.Name}   Level: {summoner.SummonerLevel}");
+        }
+    }
+}
